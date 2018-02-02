4471ee2dff6c301c7a7fbe3ce56fcc0d75e567b9
Fabricio Francisco Mendes da Silva | S2IT <fabricio.silva@S2IT.CORP>
2018-02-02 18:45:40 -0200

    Add client-side JavaScript, add progressive loading

--- homepage/server.js
+++ homepage/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const request = require('request');
+const proxy = require('http-proxy-middleware');
+const ejs = require('ejs');
+const padRight = require('pad-right');
 
 const server = express();
 
 server.set('view engine', 'ejs');
 
+// Alternative 1: use the back-end to fetch the other app
 server.get('/', (req, res) =>
   Promise
     .all([
@@ -20,10 +24,64 @@ server.get('/', (req, res) =>
     )
 );
 
-
+// Alternative 2: iFrames
 server.get('/iframe', (req, res) =>
   res.render('iframe')
 );
+
+// Alternative 3: Client-Side JavaScript
+const createProxy = (path, target) =>
+  server.use(path, proxy({ target, changeOrigin: true, pathRewrite: {[`^${path}`]: ''} }));
+
+createProxy('/header', 'https://microfrontends-header.herokuapp.com/');
+createProxy('/products-list', 'https://microfrontends-products-list.herokuapp.com/');
+createProxy('/cart', 'https://microfrontends-cart.herokuapp.com/');
+
+server.get('/client', (req, res) => res.render('client'));
+
+// Alternative 4: Progressive loading from the back-end
+server.get('/progressive', (req, res) => {
+  let appsToLoad = [
+    { name: 'header', url: 'https://microfrontends-header.herokuapp.com/' },
+    { name: 'productsList', url: 'https://microfrontends-products-list.herokuapp.com/' },
+    { name: 'cart', url: 'https://microfrontends-cart.herokuapp.com/' }
+  ]
+  let previousFlushedData = '';
+
+  const load = (piece, index) =>
+    getContents(piece.url).then(data => {
+      appsToLoad[index].data = data;
+      flushData();
+    });
+
+  const buildDataForEjs = () =>
+    appsToLoad.reduce((params, app) => {
+      params[app.name] = app.data || 'PENDING';
+      return params;
+    }, {});
+
+  const flushData = () => {
+    const dataForEjs = buildDataForEjs();
+    ejs.renderFile('views/progressive.ejs', dataForEjs, {}, (err, renderedHtml) => {
+      if (err) throw err;
+
+      const htmlToBeFlushed = renderedHtml.split('PENDING')[0].replace(previousFlushedData, '');
+
+      // chunks have to have at least 4096 bytes to make sure browsers print them
+      res.write(padRight(htmlToBeFlushed, 4096, ' '));
+      previousFlushedData += htmlToBeFlushed;
+    });
+  }
+
+  flushData();
+  Promise.all(
+    appsToLoad.map(load)
+  ).then(() =>
+    res.end()
+  );
+});
+
+// Alternative 5: WebComponents
 
 const getContents = (url) => new Promise((resolve, reject) => {
   request.get(url, (error, response, body) => {
